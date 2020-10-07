50a58412f7266a287250a837aee1534c7725e5c7
Camilla Correia <camilla.correia@cubos.io>
2020-10-06 21:27:13 -0300

    feat: upload/importação da planilha

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -101,10 +101,10 @@ const Dashboard: React.FC = () => {
 
             <tbody>
               {transactions.map(transaction => (
-                <tr>
+                <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
-                    {transaction.type === 'outcome' && '- '}
+                    {transaction.type === 'outcome' && ' - '}
                     {transaction.formattedValue}
                   </td>
                   <td>{transaction.category.title}</td>
